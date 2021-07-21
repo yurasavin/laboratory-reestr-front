ae9b75b442d854919ee12879c489cca6a6f3efd1
Yura Savin <yurasavin@gmail.com>
2021-07-21 23:48:28 +0300

    Updated timeout

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,7 +6,7 @@ import { Message, MessageBox } from 'element-ui'
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
-  timeout: 5000 // request timeout
+  timeout: 15000 // request timeout
 })
 
 // request interceptor
