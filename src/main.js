1f9d5e973540553e816312e22c6e5b39fd1956b5
Yura Savin <yurasavin@gmail.com>
2021-07-28 11:03:43 +0300

    Set ru locale

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import '@/icons' // icon
 import '@/permission' // permission control
 import '@/styles/index.scss' // global css
 import ElementUI from 'element-ui'
-import locale from 'element-ui/lib/locale/lang/en' // lang i18n
+import locale from 'element-ui/lib/locale/lang/ru-RU' // lang i18n
 import 'element-ui/lib/theme-chalk/index.css'
 import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import Vue from 'vue'
@@ -10,7 +10,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 
-// set ElementUI lang to EN
+// set ElementUI lang
 Vue.use(ElementUI, { locale })
 // 如果想要中文版 element-ui，按如下方式声明
 // Vue.use(ElementUI)
