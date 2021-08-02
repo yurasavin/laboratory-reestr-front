caf058247b5b150e99d2db0bde57358f89faf3d6
Yura Savin <yurasavin@gmail.com>
2021-08-02 15:56:06 +0300

    Removed `work_phone` field from form. Changed birthdate field logic

--- src/views/researches/components/PatientChoose.vue
+++ src/views/researches/components/PatientChoose.vue
@@ -12,15 +12,15 @@
 
       <el-form ref="form" :model="form" label-width="220px" :rules="rules">
         <el-row type="flex">
-          <el-form-item label="ФИО" prop="last_name" required>
+          <el-form-item label="ФИО" prop="last_name">
             <el-input v-model="form.last_name" placeholder="Фамилия" />
           </el-form-item>
 
-          <el-form-item prop="first_name" label-width="0" class="name-middle" required>
+          <el-form-item prop="first_name" label-width="0" class="name-middle">
             <el-input v-model="form.first_name" placeholder="Имя" />
           </el-form-item>
 
-          <el-form-item prop="middle_name" label-width="0" required>
+          <el-form-item prop="middle_name" label-width="0">
             <el-input v-model="form.middle_name" placeholder="Отчество" />
           </el-form-item>
         </el-row>
@@ -30,40 +30,9 @@
           <el-radio v-model="form.gender" :label="0">Женский</el-radio>
         </el-form-item>
 
-        <el-row type="flex">
-          <el-form-item label="Дата рождения" prop="birthdayDay" required>
-            <el-select v-model="form.birthdayDay" placeholder="День">
-              <el-option v-for="day in generateDays()" :key="day" :label="day" :value="day" />
-            </el-select>
-          </el-form-item>
-
-          <el-form-item prop="birthdayMonth" label-width="0" class="birthday" required>
-            <el-select
-              v-model="form.birthdayMonth"
-              placeholder="Месяц"
-              class="birthday-middle"
-            >
-              <el-option label="Январь" value="1" />
-              <el-option label="Февраль" value="2" />
-              <el-option label="Март" value="3" />
-              <el-option label="Апрель" value="4" />
-              <el-option label="Май" value="5" />
-              <el-option label="Июнь" value="6" />
-              <el-option label="Июль" value="7" />
-              <el-option label="Август" value="8" />
-              <el-option label="Сентябрь" value="9" />
-              <el-option label="Октябрь" value="10" />
-              <el-option label="Ноябрь" value="11" />
-              <el-option label="Декабрь" value="12" />
-            </el-select>
-          </el-form-item>
-
-          <el-form-item prop="birthdayYear" label-width="0" class="birthday" required>
-            <el-select v-model="form.birthdayYear" placeholder="Год">
-              <el-option v-for="year in generateYears()" :key="year" :label="year" :value="year" />
-            </el-select>
-          </el-form-item>
-        </el-row>
+        <el-form-item label="Дата рождения" prop="birthday" class="birthday">
+          <date-input :raw-date="form.birthday" @input="onBirthdayInput" />
+        </el-form-item>
 
         <el-form-item label="Документ">
           <el-input v-model="form.id_doc_type" placeholder="Наименование" class="document" />
@@ -81,10 +50,6 @@
 
         <el-form-item label="Контактный номер телефона">
           <el-input v-model="form.self_phone" placeholder="Телефон" class="half-width" />
-        </el-form-item>
-
-        <el-form-item label="Рабочий номер телефона">
-          <el-input v-model="form.work_phone" placeholder="Телефон" class="half-width" />
         </el-form-item>
 
         <el-form-item label="Адрес регистрации">
@@ -114,7 +79,12 @@
 </template>
 
 <script>
+import DateInput from '@/components/DateInput'
+
 export default {
+  components: {
+    DateInput
+  },
   props: {
     initialData: {
       type: Object,
@@ -129,17 +99,13 @@ export default {
         last_name: '',
         middle_name: '',
         gender: 1,
-        birthday: null,
-        birthdayDay: '',
-        birthdayMonth: '',
-        birthdayYear: '',
+        birthday: '',
         id_doc_type: '',
         id_doc_series: '',
         id_doc_num: '',
         insurance_number: '',
         insurance_company_id: '',
         self_phone: '',
-        work_phone: '',
         reg_address: '',
         fact_address: '',
         work_address: '',
@@ -156,14 +122,8 @@ export default {
         middle_name: [
           { required: true, message: 'Обязательное поле', trigger: 'blur' }
         ],
-        birthdayDay: [
-          { required: true, message: 'Выберите день', trigger: 'blur' }
-        ],
-        birthdayMonth: [
-          { required: true, message: 'Выберите месяц', trigger: 'blur' }
-        ],
-        birthdayYear: [
-          { required: true, message: 'Выберите год', trigger: 'blur' }
+        birthday: [
+          { required: true, trigger: 'blur', validator: this.birthdayValidator }
         ]
       }
     }
@@ -186,41 +146,37 @@ export default {
     for (const key in this.initialData) {
       this.form[key] = this.initialData[key]
     }
-
-    if (this.initialData.birthday) {
-      this.form.birthdayDay = this.initialData.birthday.split('-')[2]
-      this.form.birthdayMonth = this.initialData.birthday.split('-')[1]
-      this.form.birthdayYear = this.initialData.birthday.split('-')[0]
-    }
   },
   methods: {
     onClose() {
       this.$refs.form.validate((valid) => {
         const form = this.form
+
         if (valid) {
-          form.birthday = `${form.birthdayYear}-${form.birthdayMonth}-${form.birthdayDay}`
-          if (!this.$moment(form.birthday, 'YYYY-MM-DD').isValid()) {
-            this.$message({ type: 'error', message: 'Введите корректную дату!' })
-            return false
+          const dateIsValid = this.$moment(form.birthday, 'YYYY-MM-DD', true).isValid()
+          if (!dateIsValid) {
+            form.birthday = ''
+            return this.onClose()
           }
         }
+
         this.$emit('complete', form, valid)
         this.visible = false
       })
     },
-    generateDays() {
-      const range = []
-      for (let day = 1; day < 32; day++) {
-        range.push(day)
-      }
-      return range
+    onBirthdayInput(value) {
+      this.form.birthday = value
+      this.$refs.form.validateField('birthday')
     },
-    generateYears() {
-      const range = []
-      for (let year = 1940; year < 2101; year++) {
-        range.push(year)
+    birthdayValidator(rule, birthday, callback) {
+      if (!birthday) {
+        return callback(new Error('Введите дату'))
+      } else if (birthday.search('_') !== -1) {
+        return callback()
+      } else if (!this.$moment(birthday, 'YYYY-MM-DD').isValid()) {
+        return callback(new Error('Введите корректную дату'))
       }
-      return range
+      callback()
     }
   }
 }
@@ -258,10 +214,6 @@ export default {
   font: 24px bold;
 }
 
-.birthday-middle {
-  padding: 0 8px;
-}
-
 .confirm {
   display: flex;
   margin: 0 auto 20px;
@@ -269,5 +221,9 @@ export default {
 
 .patient-info {
     margin-right: 10px;
+}
+
+.birthday {
+  margin-bottom: 20px;
 }
 </style>
